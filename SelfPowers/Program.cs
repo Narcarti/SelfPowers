36be21be520810b1910e95cbd4826723ffb31e63
Narcarti <tcmarkymark@gmail.com>
2019-10-23 13:52:13 -0400

    Add files via upload

---
+++ SelfPowers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace SelfPowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UInt64 start = 1;
+
+            BigInteger sum = 0;
+
+            for (int basePow = 1; basePow < 1000; basePow++)
+            {
+                sum += BigInteger.Pow(basePow, basePow);
+            }
+
+            string last10 = "";
+            string Ssum = sum.ToString();
+            int length = Ssum.Length;
+            last10 = sum.ToString().Substring(length - 10);
+
+            Console.WriteLine(last10);
+
+            Console.ReadLine();
+        }
+        
+    }
+
+}
